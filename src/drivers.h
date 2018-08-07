c67d343b3c486ac4404d18545bb7eadad5d2e214
Patrick O'Banion <pobanion@live.com>
2018-08-06 20:04:34 -0500

    Header for different device drivers.
    Functions used for enabling devices.

---
+++ src/drivers.h
@@ -0,0 +1,7 @@
+extern void enBlue(void);
+extern void enMoto(void);
+extern void enAnal(void);
+extern void enLED(void);
+extern void UARTIntHandler(void);
+extern int infraRed(void);
+extern void enTimer(void);
