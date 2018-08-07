c1eb040609fa642e646e7c181501cfdf6af10922
Patrick O'Banion <pobanion@live.com>
2018-08-06 20:00:55 -0500

    Header file for basic movement functions.
    Functions for controlling different devices. Currently can control motors dirrections and speed, and take analog  distance measurements.

---
+++ src/com.h
@@ -0,0 +1,7 @@
+extern int lookUpCom (char *com);
+extern void analoRead(void);
+extern void revMoto0(void);
+extern void revMoto1(void);
+extern void forMoto0(void);
+extern void forMoto1(void);
+extern void setSpeed(int speed0, int speed1);
