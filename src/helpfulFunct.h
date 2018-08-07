1d2391ea7926701c9595abef025554e19fee9729
Patrick O'Banion <pobanion@live.com>
2018-08-06 20:08:46 -0500

    Header for ect. functions.
    Most functions are for string handling or simple math. Functions were moved from main so other translation units could utilize them. Also to decrease compilation times.

---
+++ src/helpfulFunct.h
@@ -0,0 +1,5 @@
+extern void SCI_OutUDec(unsigned short n);
+extern int readInputInt(int size);
+extern char *readInputChar(int size);
+extern float pError(int act, int theor);
+extern void printString(char *string);
